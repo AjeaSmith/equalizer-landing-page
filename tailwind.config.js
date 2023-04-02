7c8ce9f70551ae6f19dd094a666dfb0fbf132f83
ajea <smithajea@rocketmail.com>
2023-04-02 04:25:15 -0400

    configured tailwindcss with custom styles from design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        darkPurple: 'hsl(273, 85%, 13%)',
-        lightPurple: 'hsl(271, 12%, 34%)',
-        veryLightPurple: 'hsl(276, 100%, 99%)',
-        white: 'hsl(0, 0%, 100%)',
-        green: 'hsl(150, 100%, 63%)'
+        teal: '#66E2DC',
+        darkOrange: '#FA7453',
+        lightOrange: '#FFB964',
+        white: '#FCFAF9',
+        black: '#191826'
       },
-    },
+      screens: {
+      'tablet': '1023px',
+      'desktop': '1439px',
+      },
+      lineHeight: {
+        '26': '1.625rem',
+        '34': '2.125rem',
+        '52': '3.25rem',
+        '88': '5.5rem',
+      }
+    }
   },
   plugins: [],
 }

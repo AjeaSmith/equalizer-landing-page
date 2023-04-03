f2d397959a0f4219d99414e0f3a8754bfa43f6c7
ajea <smithajea@rocketmail.com>
2023-04-03 07:35:49 -0400

    worked on making header section responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,11 @@ module.exports = {
       },
       lineHeight: {
         '26': '1.625rem',
+        '28': '1.75rem',
         '34': '2.125rem',
+        '48': '3rem',
         '52': '3.25rem',
+        '64': '4rem',
         '88': '5.5rem',
       },
       backgroundImage: {
@@ -25,6 +28,20 @@ module.exports = {
         'md-image': "url('/assets/bg-main-tablet.png')",
         'lg-image': "url('/assets/bg-main-desktop.png')"
       }
+    },
+    fontSize: {
+      40: '2.5rem',
+      32: '2rem',
+      64: '4rem',
+      88: '5.5rem',
+      sm: '0.8rem',
+      lg: '1.125rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
     }
   },
   plugins: [],
